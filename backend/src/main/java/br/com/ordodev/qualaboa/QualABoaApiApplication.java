c2b345fdd70bcb6860c08d7ccee113dcaf631979
João D'Araujo <joaofernandes.daraujo@gmail.com>
2026-09-11 01:28:31 -0300

    esqueleto do backend e do frontend

---
+++ backend/src/main/java/br/com/ordodev/qualaboa/QualABoaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ordodev.qualaboa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualABoaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualABoaApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/ordodev/qualaboa/QualABoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ordodev.qualaboa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualABoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
